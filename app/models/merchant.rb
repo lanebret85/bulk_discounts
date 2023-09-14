2468642604187ce672a58b82a7f2852c3d82e621
Dylan Timmons <dylanstimmons@icloud.com>
2023-09-13 18:29:16 -0600

    Add top 5 customer method

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -3,4 +3,11 @@ class Merchant < ApplicationRecord
   
   has_many :items
   has_many :invoices, through: :items
+  has_many :transactions, through: :invoices
+  has_many :customers, through: :invoices
+
+  def top_5_customers
+    Customer.select('customers.*, COUNT(transactions.id) as success_transactions').joins(invoices: :transactions).where(transactions: { result: "success" }).group('customers.id').order('success_transactions DESC').limit(5)
+  end
+      
 end
