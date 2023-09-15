c75341f6e32cc0693eeba1abf3e965e29b40a7e7
Dylan Timmons <dylanstimmons@icloud.com>
2023-09-15 17:44:14 -0600

    Add test for full name method

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -4,4 +4,14 @@ RSpec.describe Customer, type: :model do
   describe "relationships" do
     it { should have_many(:invoices) }
   end
+
+  describe "full_name" do
+    it "returns the customers full name" do
+      customer1 = Customer.create!(first_name: "Eddie", last_name: "Izzard")
+      customer2 = Customer.create!(first_name: "Joe", last_name: "Franco")
+
+      expect(customer1.full_name).to eq("Eddie Izzard")
+      expect(customer2.full_name).to eq("Joe Franco")
+    end
+  end
 end
