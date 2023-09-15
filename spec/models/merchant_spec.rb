c9c87476cbddfc2d47c9d5f2041cb5acb2231629
Dylan Timmons <dylanstimmons@icloud.com>
2023-09-15 13:35:05 -0600

    Add test for item to be shipped method

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -12,45 +12,66 @@ RSpec.describe Merchant, type: :model do
     it { should define_enum_for(:status).with_values(["enabled", "disabled"])}
   end
 
+  before(:each) do
+    @merchant1 = Merchant.create!(name: "BOB BURGER SHOP")
+    @merchant2 = Merchant.create!(name: "Cindy's Cookies")
+
+    @customer1 = Customer.create(first_name: "Cindy", last_name: "Loo")
+    @customer2 = Customer.create(first_name: "Steve", last_name: "Boo")
+    @customer3 = Customer.create(first_name: "Joshil", last_name: "Moo")
+    @customer4 = Customer.create(first_name: "Jon", last_name: "Stu")
+    @customer5 = Customer.create(first_name: "Sarah", last_name: "Who")
+    @customer6 = Customer.create(first_name: "Chandni", last_name: "Sue")
+    @customer7 = Customer.create(first_name: "Dani", last_name: "James")
+    @customer8 = Customer.create(first_name: "Ryan", last_name: "Howard")
+      
+    @item1 = Item.create!(name: "Burger", unit_price: 15, merchant_id: @merchant1.id)
+    @item2 = Item.create!(name: "Soda", unit_price: 7, merchant_id: @merchant1.id)
+    @item3 = Item.create!(name: "Cookie", unit_price: 70, merchant_id: @merchant2.id)
+    @item4 = Item.create!(name: "Big Cookie", unit_price: 7090, merchant_id: @merchant2.id)
+      
+    @invoice1 = Invoice.create!(status: 0, customer_id: @customer1.id)
+    @invoice2 = Invoice.create!(status: 1, customer_id: @customer2.id)
+    @invoice3 = Invoice.create!(status: 1, customer_id: @customer3.id)
+    @invoice4 = Invoice.create!(status: 1, customer_id: @customer4.id)
+    @invoice5 = Invoice.create!(status: 1, customer_id: @customer5.id)
+    @invoice6 = Invoice.create!(status: 1, customer_id: @customer6.id)
+    @invoice7 = Invoice.create!(status: 1, customer_id: @customer7.id)
+    @invoice8 = Invoice.create!(status: 1, customer_id: @customer8.id)
+      
+    @invoice_item1 = InvoiceItem.create!(item_id: @item1.id, invoice_id: @invoice1.id, quantity: 1, unit_price: 345, status: 1) 
+    @invoice_item2 = InvoiceItem.create!(item_id: @item2.id, invoice_id: @invoice2.id, quantity: 1, unit_price: 345, status: 0) 
+    @invoice_item3 = InvoiceItem.create!(item_id: @item1.id, invoice_id: @invoice3.id, quantity: 1, unit_price: 345, status: 1) 
+    @invoice_item4 = InvoiceItem.create!(item_id: @item2.id, invoice_id: @invoice4.id, quantity: 1, unit_price: 345, status: 1) 
+    @invoice_item5 = InvoiceItem.create!(item_id: @item1.id, invoice_id: @invoice5.id, quantity: 1, unit_price: 345, status: 1) 
+    @invoice_item6 = InvoiceItem.create!(item_id: @item2.id, invoice_id: @invoice6.id, quantity: 1, unit_price: 345, status: 2)
+    @invoice_item7 = InvoiceItem.create!(item_id: @item3.id, invoice_id: @invoice7.id, quantity: 1, unit_price: 345, status: 1)
+    @invoice_item8 = InvoiceItem.create!(item_id: @item4.id, invoice_id: @invoice8.id, quantity: 1, unit_price: 345, status: 2)
+
+    @transaction1 = Transaction.create!(invoice_id: @invoice1.id, credit_card_number: "1234567812345678", credit_card_expiration_date: "10/26", result: 1)
+    @transaction2 = Transaction.create!(invoice_id: @invoice2.id, credit_card_number: "1234567812345678", credit_card_expiration_date: "10/26", result: 0)
+    @transaction3 = Transaction.create!(invoice_id: @invoice3.id, credit_card_number: "1234567812345678", credit_card_expiration_date: "10/26", result: 0)
+    @transaction4 = Transaction.create!(invoice_id: @invoice4.id, credit_card_number: "1234567812345678", credit_card_expiration_date: "10/26", result: 0)
+    @transaction5 = Transaction.create!(invoice_id: @invoice5.id, credit_card_number: "1234567812345678", credit_card_expiration_date: "10/26", result: 0)
+    @transaction6 = Transaction.create!(invoice_id: @invoice6.id, credit_card_number: "1234567812345678", credit_card_expiration_date: "10/26", result: 0)
+  end
+
   describe "top_5_customers" do
     it "displays the top 5 customers" do
-      merchant1 = Merchant.create!(name: "BOB BURGER SHOP")
-
-      customer1 = Customer.create(first_name: "Cindy", last_name: "Loo")
-      customer2 = Customer.create(first_name: "Steve", last_name: "Boo")
-      customer3 = Customer.create(first_name: "Joshil", last_name: "Moo")
-      customer4 = Customer.create(first_name: "Jon", last_name: "Stu")
-      customer5 = Customer.create(first_name: "Sarah", last_name: "Who")
-      customer6 = Customer.create(first_name: "Chandni", last_name: "Sue")
       
-      item1 = Item.create!(name: "Burger", unit_price: 15, merchant_id: merchant1.id)
-      item2 = Item.create!(name: "Soda", unit_price: 7, merchant_id: merchant1.id)
-      
-      invoice1 = Invoice.create!(status: 0, customer_id: customer1.id)
-      invoice2 = Invoice.create!(status: 1, customer_id: customer2.id)
-      invoice3 = Invoice.create!(status: 1, customer_id: customer3.id)
-      invoice4 = Invoice.create!(status: 1, customer_id: customer4.id)
-      invoice5 = Invoice.create!(status: 1, customer_id: customer5.id)
-      invoice6 = Invoice.create!(status: 1, customer_id: customer6.id)
-      
-      invoice_item1 = InvoiceItem.create!(item_id: item1.id, invoice_id: invoice1.id, quantity: 1, unit_price: 345, status: 1) 
-      invoice_item2 = InvoiceItem.create!(item_id: item2.id, invoice_id: invoice2.id, quantity: 1, unit_price: 345, status: 0) 
-      invoice_item3 = InvoiceItem.create!(item_id: item1.id, invoice_id: invoice3.id, quantity: 1, unit_price: 345, status: 1) 
-      invoice_item4 = InvoiceItem.create!(item_id: item2.id, invoice_id: invoice4.id, quantity: 1, unit_price: 345, status: 1) 
-      invoice_item5 = InvoiceItem.create!(item_id: item1.id, invoice_id: invoice5.id, quantity: 1, unit_price: 345, status: 1) 
-      invoice_item6 = InvoiceItem.create!(item_id: item2.id, invoice_id: invoice6.id, quantity: 1, unit_price: 345, status: 2) 
+      top_5 = @merchant1.top_5_customers
 
-      transaction1 = Transaction.create!(invoice_id: invoice1.id, credit_card_number: "1234567812345678", credit_card_expiration_date: "10/26", result: 1)
-      transaction2 = Transaction.create!(invoice_id: invoice2.id, credit_card_number: "1234567812345678", credit_card_expiration_date: "10/26", result: 0)
-      transaction3 = Transaction.create!(invoice_id: invoice3.id, credit_card_number: "1234567812345678", credit_card_expiration_date: "10/26", result: 0)
-      transaction4 = Transaction.create!(invoice_id: invoice4.id, credit_card_number: "1234567812345678", credit_card_expiration_date: "10/26", result: 0)
-      transaction5 = Transaction.create!(invoice_id: invoice5.id, credit_card_number: "1234567812345678", credit_card_expiration_date: "10/26", result: 0)
-      transaction6 = Transaction.create!(invoice_id: invoice6.id, credit_card_number: "1234567812345678", credit_card_expiration_date: "10/26", result: 0)
+      expect(top_5).to eq([@customer2, @customer3, @customer4, @customer5, @customer6])
+      expect(top_5).to_not include(@customer1)
+    end
+  end
 
-      top_5 = merchant1.top_5_customers
-
-      expect(top_5).to eq([customer2, customer3, customer4, customer5, customer6])
-      expect(top_5).to_not include(customer1)
+  describe "items_to_be_shipped" do
+    it "should return only items that have not been shipped yet" do
+      items_to_ship = @merchant2.item_to_be_shipped
+  
+      expect(items_to_ship).to include(@item3)
+      expect(items_to_ship).not_to include(@item4)
     end
   end
 end
