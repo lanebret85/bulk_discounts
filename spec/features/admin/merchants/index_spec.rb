d3547fa0b0362234cfc054608536f885d9623b3e
Lane Bretschneider <lanebret85@gmail.com>
2023-09-15 09:35:56 -0700

    test: add test for link to create a new merchant

--- spec/features/admin/merchants/index_spec.rb
+++ spec/features/admin/merchants/index_spec.rb
@@ -20,39 +20,45 @@ RSpec.describe "Admin Merchants Index", type: :feature do
         
         visit admin_merchants_path
 
-        expect(page).to have_button("Disable #{@merchant_1.name}")
-
-        click_button "Disable #{@merchant_1.name}"
-        
-        expect(current_path).to eq(admin_merchants_path)
-
         expect(page).to have_button("Enable #{@merchant_1.name}")
-        expect(page).not_to have_button("Disable #{@merchant_1.name}")
 
         click_button "Enable #{@merchant_1.name}"
-
+        
         expect(current_path).to eq(admin_merchants_path)
 
         expect(page).to have_button("Disable #{@merchant_1.name}")
         expect(page).not_to have_button("Enable #{@merchant_1.name}")
+
+        click_button "Disable #{@merchant_1.name}"
+
+        expect(current_path).to eq(admin_merchants_path)
+
+        expect(page).to have_button("Enable #{@merchant_1.name}")
+        expect(page).not_to have_button("Disable #{@merchant_1.name}")
       end
     end
 
-    it "I see two sections, 'Enabled Merchants' and 'Disables Merchants' with the appropriate merchants in each section" do
+    it "I see two sections, 'Enabled Merchants' and 'Disabled Merchants' with the appropriate merchants in each section" do
       @merchant_1 = create(:merchant)
       @merchant_2 = create(:merchant)
 
       visit admin_merchants_path
 
-      click_button "Disable #{@merchant_2.name}"
+      click_button "Enable #{@merchant_2.name}"
 
       within("#enabled-merchants") do
-        expect(page).to have_content(@merchant_1.name)
+        expect(page).to have_content(@merchant_2.name)
       end
 
       within("#disabled-merchants") do
-        expect(page).to have_content(@merchant_2.name)
+        expect(page).to have_content(@merchant_1.name)
       end
+    end
+
+    it "I see a link to create a new merchant, and when I click the link I am taken to a form to add merchant info" do
+      visit admin_merchants_path
+
+      expect(page).to have_link "Create a new merchant", href: "/admin/merchants/new"
     end
   end
 end
