be0c2801e89c4d90b42a37dc8b21d1990f6da9e4
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2023-09-16 14:06:48 -0500

    Add the update method to the items_controller and the private item_params method

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -12,4 +12,23 @@ class ItemsController < ApplicationController
       @merchant = Merchant.find(params[:merchant_id])
       @item = Item.find(params[:item_id])
    end
+
+   def update
+      @merchant = Merchant.find(params[:merchant_id])
+      @item = Item.find(params[:item_id])
+      # require 'pry';binding.pry
+      if @item.update(item_params)
+         redirect_to "/merchants/#{@merchant.id}/items/#{@item.id}"
+         flash[:success] = "Item information updated successfully."
+      else
+         redirect_to "/merchants/#{@merchant.id}/items/#{@item.id}/edit"
+         flash[:alert] = "Not all updates were saved, please try again."
+      end
+   end
+
+   private 
+
+   def item_params
+      params.require(:item).permit(:name, :description, :unit_price, :other_attributes)
+    end
 end
