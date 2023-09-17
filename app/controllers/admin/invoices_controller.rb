e6bd20ebc860f966fa9fc309fa53830532f30b93
Eliza Keating <ek.elizakeating@gmail.com>
2023-09-16 20:14:42 -0600

    feat: add update method to admin invoices controller

--- app/controllers/admin/invoices_controller.rb
+++ app/controllers/admin/invoices_controller.rb
@@ -6,4 +6,11 @@ class Admin::InvoicesController < ApplicationController
   def show
     @invoice = Invoice.find(params[:invoice_id])
   end
+
+  def update
+    invoice = Invoice.find(params[:invoice_id])
+    invoice.update!(status: params[:status])
+
+    redirect_to "/admin/invoices/#{invoice.id}"
+  end
 end
