6344ad18bdf0707264f74880a41fd2c5c0ac5747
Dylan Timmons <dylanstimmons@icloud.com>
2023-09-16 18:17:51 -0600

    Adds total revenue method

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -6,5 +6,8 @@ class Invoice < ApplicationRecord
   has_many :invoice_items
   has_many :items, through: :invoice_items
 
+  def total_revenue
+   invoice_items.sum('quantity*unit_price')
+  end
 
 end
