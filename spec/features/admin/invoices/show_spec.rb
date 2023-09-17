b0d8e77126be73ee56fb5d26e8f3cbb1f103944c
Eliza Keating <ek.elizakeating@gmail.com>
2023-09-16 19:50:27 -0600

    test: add test for US 35

--- spec/features/admin/invoices/show_spec.rb
+++ spec/features/admin/invoices/show_spec.rb
@@ -14,5 +14,23 @@ RSpec.describe "Admin Invoice Show Page", type: :feature do
       expect(page).to have_content("Created: Sunday, March 25, 2012")
       expect(page).to have_content("Customer: #{customer_1.first_name} #{customer_1.last_name}")
     end
+
+    it "I see the total revenue that will be generated from this invoice" do
+      customer_1 = create(:customer)
+
+      invoice_1 = create(:invoice, customer: customer_1)
+
+      merchant_1 = create(:merchant)
+
+      item_1 = create(:item, merchant: merchant_1)
+
+      invoice_item_1 = create(:invoice_item, quantity: 5, unit_price: 24642, invoice: invoice_1, item: item_1)
+      invoice_item_2 = create(:invoice_item, quantity: 8, unit_price: 12463, invoice: invoice_1, item: item_1)
+      invoice_item_2 = create(:invoice_item, quantity: 2, unit_price: 23567, invoice: invoice_1, item: item_1)
+
+      visit "/admin/invoices/#{invoice_1.id}"
+
+      expect(page).to have_content("Total Revenue: $2,700.48")
+    end
   end
 end
