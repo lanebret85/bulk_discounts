ad9774c8ff88cb789de717ac530cdb8c8cab85db
Eliza Keating <ek.elizakeating@gmail.com>
2023-09-17 12:13:48 -0600

    test: add test for find_items class method of invoice item

--- spec/models/invoice_item_spec.rb
+++ spec/models/invoice_item_spec.rb
@@ -15,4 +15,31 @@ RSpec.describe InvoiceItem, type: :model do
     it { should validate_presence_of :unit_price }
     it { should validate_presence_of :status }
   end
+
+  describe ".find_items" do
+    it "returns the name of an item associated with an invoice item, as well as the invoice items quantity, unit price, and status" do
+      customer_1 = create(:customer)
+
+      invoice_1 = create(:invoice, customer: customer_1)
+
+      merchant_1 = create(:merchant)
+
+      item_1 = create(:item, name: "Item 1", merchant: merchant_1)
+
+      invoice_item_1 = InvoiceItem.create!(
+        item: item_1,
+        invoice: invoice_1,
+        quantity: 2,
+        unit_price: 36742,
+        status: "shipped"
+      )
+
+      item = InvoiceItem.find_items(invoice_1)
+
+      expect(item.first.name).to eq("Item 1")
+      expect(item.first.quantity).to eq(2)
+      expect(item.first.unit_price).to eq(36742)
+      expect(item.first.status).to eq("shipped")
+    end
+  end
 end
