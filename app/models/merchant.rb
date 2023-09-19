3142ae88003c3a185b35246dbb71002040fd451d
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2023-09-19 18:28:04 -0500

    Create the 'top_5_items' method query for US 12

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -12,6 +12,10 @@ class Merchant < ApplicationRecord
   def top_5_customers
     Customer.select('customers.*, COUNT(transactions.id) as success_transactions').joins(invoices: :transactions).where(transactions: { result: "success" }).group('customers.id').order('success_transactions DESC').limit(5)
   end
+
+  def top_5_items
+    Item.select("items.*, SUM(invoice_items.quantity * invoice_items.unit_price) AS revenue").joins(invoice_items: { invoice: :transactions }).where(transactions: { result: "success" }).group("items.id").order("revenue DESC").limit(5)
+  end
       
   def item_to_be_shipped
     Item.select("items.*, invoice_items.invoice_id, invoices.created_at").joins(invoice_items: :invoice).where.not(invoice_items: {status: 2 }).order("invoices.created_at ASC")
