db1957f820b83f06105442b269442217c207d6bb
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2023-09-19 20:03:34 -0500

    Create the item_revenue method for the merchant model to calculate the total revenue of each item

--- spec/features/merchant/items/index_spec.rb
+++ spec/features/merchant/items/index_spec.rb
@@ -154,9 +154,23 @@ RSpec.describe "Merchant Items Index" do
     it "Displays the names of the top 5 most popular items ranked by total revenue generated" do
       expect(page).to have_content("Top 5 Most Popular Items:")
       save_and_open_page
-      # within("div.top-5-items") do
-      #   # expect(page).to have_content()
-      # end
+      within("div.top-5-items") do
+        expect(page).to_not have_content("#{@item3.name}")
+        expect(page).to_not have_content("#{@item4.name}")
+        expect(page).to_not have_content("#{@item5.name}")
+        expect(page).to_not have_content("#{@item6.name}")
+        expect(page).to_not have_content("#{@item7.name}")
+        expect(page).to have_content("#{@item2.name}")
+        expect(page).to have_content("#{@item1.name}")
+        expect(page).to have_content("#{@item8.name}")
+        expect(page).to have_content("#{@item9.name}")
+        expect(page).to have_content("#{@item10.name}")
+
+        expect("#{@item2.name}").to appear_before("#{@item1.name}")
+        expect("#{@item1.name}").to appear_before("#{@item8.name}")
+        expect("#{@item8.name}").to appear_before("#{@item9.name}")
+        expect("#{@item9.name}").to appear_before("#{@item10.name}")
+      end
     end
 
     xit "Each item name links to my merchant item show page for that item" do
