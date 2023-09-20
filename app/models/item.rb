56a272cf3b6fc17897a7726570740818af7ee49a
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2023-09-20 00:47:41 -0500

    Create the 'item_best_day' method for US 13

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,4 +8,8 @@ class Item < ApplicationRecord
   validates :name, presence: true
   validates :description, presence: true
   validates :unit_price, presence: true
+
+  def self.item_best_days(item)
+    select("MAX(invoice_items.quantity * invoice_items.unit_price) AS total_revenue, DATE(invoices.created_at) AS date").joins(invoice_items: { invoice: :transactions }).where("items.id = ?", item.id).group(:date).order("total_revenue DESC").first.date
+  end
 end
