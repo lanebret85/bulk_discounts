beef05a5f2cae3cf6bb26758045f800405eb4313
Lane Bretschneider <lanebret85@gmail.com>
2023-09-19 17:12:43 -0700

    refactor: remove unncessary before each

--- spec/features/admin/index_spec.rb
+++ spec/features/admin/index_spec.rb
@@ -1,10 +1,6 @@
 require "rails_helper"
 
 RSpec.describe "Admin Dashboard", type: :feature do
-  before :each do
-    
-  end
-
   describe "When I visit the admin dashboard" do
     it "displays a header 'Admin Dashboard'" do
       visit "/admin"
