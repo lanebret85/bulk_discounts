9cd219865b3abff52032791c9920f437b4885d51
Lane Bretschneider <lanebret85@gmail.com>
2023-09-22 13:35:40 -0700

    test: add tests for associations to merchant_bulk_discounts and bulk_discounts through that, add test for validation of name attribute

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -6,6 +6,12 @@ RSpec.describe Merchant, type: :model do
     it { should have_many(:invoices).through :items }
     it { should have_many(:customers).through :invoices }
     it { should have_many(:transactions).through :invoices }
+    it { should have_many :merchant_bulk_discounts }
+    it { should have_many(:bulk_discounts).through(:merchant_bulk_discounts) }
+  end
+
+  describe "validations" do
+    it { should validate_presence_of :name }
   end
 
   describe "enum" do
