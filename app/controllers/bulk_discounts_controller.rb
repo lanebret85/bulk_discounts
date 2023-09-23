cef98351f108a0fd5d7792bc42b5a3a893c3a37b
Lane Bretschneider <lanebret85@gmail.com>
2023-09-22 21:13:47 -0700

    feat: add new and create actions

--- app/controllers/bulk_discounts_controller.rb
+++ app/controllers/bulk_discounts_controller.rb
@@ -4,6 +4,18 @@ class BulkDiscountsController < ApplicationController
   end
 
   def show
-    
+
+  end
+
+  def new
+    @merchant = Merchant.find(params[:merchant_id])
+  end
+  
+  def create
+    merchant = Merchant.find(params[:merchant_id])
+    bulk_discount = BulkDiscount.create!(quantity_threshold: params[:quantity_threshold], percentage_discount: params[:percentage_discount], description: "#{((params[:percentage_discount]).to_f*100).to_i}% off #{params[:quantity_threshold]} items or more")
+    redirect_to "/merchants/#{merchant.id}/bulk_discounts/#{bulk_discount.id}/create"
+    # MerchantBulkDiscount.create!(merchant: merchant, bulk_discount: bulk_discount)
+    # redirect_to "/merchants/#{merchant.id}/bulk_discounts"
   end
 end
