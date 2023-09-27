353f54e1f787cd3f730587bc5d5f03f8dab79689
Lane Bretschneider <lanebret85@gmail.com>
2023-09-26 17:52:46 -0700

    feat: add discount_applied? and applied_discount methods

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -7,4 +7,31 @@ class InvoiceItem < ApplicationRecord
   validates :quantity, presence: true
   validates :unit_price, presence: true
   validates :status, presence: true
+
+  def discount_applied?
+    if self.item.merchant.bulk_discounts != []
+      invoice_items = Invoice.joins(:bulk_discounts)
+                              .select("min(bulk_discounts.quantity_threshold) as min_threshold")
+                              .where("#{self.invoice_id} = invoices.id")
+                              .order("bulk_discounts.quantity_threshold ASC")
+                              .group("bulk_discounts.id")
+      if self.quantity >= invoice_items.first.min_threshold
+        true
+      else
+        false
+      end
+    else
+      false
+    end
+  end
+
+  def applied_discount
+    discounts = Invoice.joins(:bulk_discounts)
+                        .select("bulk_discounts.id, max(bulk_discounts.percentage_discount)")
+                        .where("#{self.quantity} >= bulk_discounts.quantity_threshold")
+                        .order("bulk_discounts.percentage_discount DESC")
+                        .group("bulk_discounts.id")
+      
+    discounts.first.id
+  end
 end
